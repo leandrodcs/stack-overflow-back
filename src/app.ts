081e47284b71d7e7ac78cd954fb74636c0da21cb
leandrodcs <eng.leandrodcs@gmail.com>
2021-12-10 14:05:54 -0300

    Setting up env and first route

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import userRouter from './routers/userRouter';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use('/users', userRouter);
 
 app.get('/health', (req, res) => {
     res.sendStatus(200);
